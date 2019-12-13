e4981555205a62bdcad7775cf00d568abf5c015f
jeonghee <31604468+jh170904@users.noreply.github.com>
2019-12-13 16:56:36 +0900

    12/13 자동설정 재정의 @Conditional 어노테이션

---
+++ Chapter02/src/main/java/com/studyboot/config/BoardConfiguration.java
@@ -0,0 +1,21 @@
+package com.studyboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.studyboot.jdbc.util.JDBCConnectionManager;
+
+@Configuration
+public class BoardConfiguration {
+	
+	// 자동설정한 내용이 아닌 DB설정을 변경하고 싶을 경우 
+	@Bean
+	public JDBCConnectionManager getJDBConnectionManager() {
+		JDBCConnectionManager manager = new JDBCConnectionManager();
+		manager.setDriverClass("org.h2.Driver");
+		manager.setUrl("jdbc:h2:tcp://localhost/~/test");
+		manager.setUsername("h2");
+		manager.setPassword("h2");
+		return manager;
+	}
+}
---
+++ Chapter02/src/main/java/com/studyboot/jdbc/util/JDBCConnectionManagerRunner.java
@@ -0,0 +1,18 @@
+package com.studyboot.jdbc.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JDBCConnectionManagerRunner implements ApplicationRunner {
+	
+	@Autowired
+	private JDBCConnectionManager connectionManager;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("커넥션 매니저" + connectionManager.toString());
+	}
+}
