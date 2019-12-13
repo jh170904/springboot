2e5b3b8e3141ca76e9ba7d7cf831d553e87fe380
이정희 <@jeonghee>
2019-12-13 17:23:33 +0900

    12/13 자동설정 재정의 (프로퍼티 파일 이용)

--- board-spring-boot-starter/src/main/java/com/studyboot/jdbc/config/BoardAutoConfiguration.java
+++ board-spring-boot-starter/src/main/java/com/studyboot/jdbc/config/BoardAutoConfiguration.java
@@ -1,6 +1,8 @@
 package com.studyboot.jdbc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,16 +10,21 @@ import com.studyboot.jdbc.util.JDBCConnectionManager;
 
 // @Configuration을 가지고 있으므로 자동으로 빈 등록. 빈 등록시 @Bean설정들도 객체로 등록
 @Configuration
+// 활성화할 프로퍼티 클래스를 지정할 때 사용 
+@EnableConfigurationProperties(JDBCConncetionManagerProperties.class)
 public class BoardAutoConfiguration {
 
+	@Autowired // 의존성 주입하여 사용가능
+	private JDBCConncetionManagerProperties properties;
+	
 	@Bean
 	@ConditionalOnMissingBean
 	public JDBCConnectionManager getJdbcConnectionManager() {
 		JDBCConnectionManager manager = new JDBCConnectionManager();
-		manager.setDriverClass("oracle.jdbc.driver.OracleDriver");
-		manager.setUrl("jdbc::oracle:thin:@localhost:1521:xe");
-		manager.setUsername("window");
-		manager.setPassword("window");
+		manager.setDriverClass(properties.getDriverClass());
+		manager.setUrl(properties.getUrl());
+		manager.setUsername(properties.getUsername());
+		manager.setPassword(properties.getPassword());
 		
 		return manager;
 	}
