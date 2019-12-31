f9175f08b97df635b0ab787629ce9e11a788cd1d
jeonghee <31604468+jh170904@users.noreply.github.com>
2019-12-31 11:22:21 +0900

    12/31 Query Annotation Test case

--- Chapter05/src/main/java/com/studyboot/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/studyboot/persistence/BoardRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.studyboot.domain.Board;
 
@@ -27,17 +28,26 @@ public interface BoardRepository extends CrudRepository<Board, Long>{
 	
 	// 페이징 처리
 	Page<Board> findBoardByTitleContaining(String searchKeyword, Pageable paging);
-	
+
 	// 위치 기반 파라미터 사용
-	@Query("SELECT b FROM Board b WHERE b.title LIKE %?1% ORDER BY b.seq DESC")
-	List<Board> queryAnnotationTest1(String searchKeyword);
-	
-	// 이름 기반 파라미터 사용
-	//@Query("SELECT b FROM Board b WHERE b.title LIKE %:searchKeyword% ORDER BY b.seq DESC")
+	//@Query("SELECT b FROM Board b WHERE b.title LIKE %?1% ORDER BY b.seq DESC")
 	//List<Board> queryAnnotationTest1(String searchKeyword);
 	
+	// 이름 기반 파라미터 사용
+	@Query("SELECT b FROM Board b WHERE b.title LIKE %:searchKeyword% ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest1(String searchKeyword);
+	
 	// 특정변수만 조회하기
-	//@Query("SELECT b.seq, b.title, b.writer, b.createDate FROM Board b WHERE b.title LIKE %?1% ORDER BY b.seq DESC")
-	//List<Object[]> queryAnnotationTest2(@Param("searchKeyword") String searchKeyword);
-
+	@Query("SELECT b.seq, b.title, b.writer, b.createDate FROM Board b WHERE b.title LIKE %?1% ORDER BY b.seq DESC")
+	List<Object[]> queryAnnotationTest2(@Param("searchKeyword") String searchKeyword);
+	
+	// 네이티브 쿼리 (엔티티 기준이 아니라 컬럼명 기준이므로 주의하자)
+	@Query(value="SELECT seq, title, writer, create_date FROM Board WHERE title LIKE '%'||?1||'%' ORDER BY seq DESC"
+			,nativeQuery = true)
+	List<Object[]> queryAnnotationTest3(String searchKeyword);	
+	
+	// 페이징 및 정렬 사용
+	@Query("SELECT b FROM Board b ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest4(Pageable paging);
+	
 }
--- Chapter05/src/test/java/com/studyboot/QueryAnnotationTest.java
+++ Chapter05/src/test/java/com/studyboot/QueryAnnotationTest.java
@@ -1,11 +1,16 @@
 package com.studyboot;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,9 +24,38 @@ public class QueryAnnotationTest {
 	@Autowired
 	private BoardRepository boardRepo;
 	
-	@Test
+	//@Test
 	public void testQueryAnnotationTest1() {
 		List<Board> boardList = boardRepo.queryAnnotationTest1("테스트 제목10");
+		System.out.println("검색 결과");
+		for(Board b : boardList){
+			System.out.println("--------> "+b.toString());
+		}
+	}
+	
+	//@Test
+	public void testQueryAnnotationTest2() {
+		List<Object[]> boardList = boardRepo.queryAnnotationTest2("테스트 제목10");
+		System.out.println("검색 결과");
+		for(Object[] row : boardList){
+			System.out.println("--------> "+Arrays.toString(row));
+		}
+	}
+	
+	//@Test
+	public void testQueryAnnotationTest3() {
+		List<Object[]> boardList = boardRepo.queryAnnotationTest3("테스트 제목10");
+		System.out.println("검색 결과");
+		for(Object[] row : boardList){
+			System.out.println("--------> "+Arrays.toString(row));
+		}
+	}
+	
+	@Test
+	public void testQueryAnnotationTest4() {
+		Pageable paging  = PageRequest.of(0, 3, Sort.Direction.DESC, "seq");
+		//첫번째 페이지부터 세개의 데이터씩, 정렬은 seq기준으로 내림차순
+		List<Board> boardList = boardRepo.queryAnnotationTest4(paging);
 		System.out.println("검색 결과");
 		for(Board b : boardList){
 			System.out.println("--------> "+b.toString());
